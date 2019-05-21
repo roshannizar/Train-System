6f9dae5e5ed1a52659267821273927ba8033064b
roshannizar <roshannizar8@gmail.com>
2019-05-21 09:04:54 +0530

    Cart Action: Delete

--- client/src/action/CartActions.js
+++ client/src/action/CartActions.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-import { TRAIN_LOADING, GET_TRAINS } from './types';
+import { TRAIN_LOADING, GET_TRAINS, GET_ERRORS } from './types';
 
 export const getBookings = () => dispatch => {
     dispatch(setTrainLoading());
@@ -11,6 +11,14 @@ export const getBookings = () => dispatch => {
     .catch(err => dispatch({
         type: GET_TRAINS,
         payload: null
+    }));
+};
+
+export const deleteBooking = () => dispatch => {
+    dispatch(setTrainLoading());
+    axios.delete('/api/bookings/remove').then(res => console.log('Removed')).catch(err => dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
     }));
 };
 
--- client/src/component/cart/CartItem.js
+++ client/src/component/cart/CartItem.js
@@ -1,11 +1,43 @@
+/* eslint-disable react/no-direct-mutation-state */
 /* eslint-disable jsx-a11y/img-redundant-alt */
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-
+import { deleteBooking } from '../../action/CartActions';
+import { connect } from 'react-redux';
 import Pic from '../../images/train.png';
 import '../landing/style.css';
 
 class CartItem extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            id: '',
+            errors:{}
+        };
+        this.onChange = this.onChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if(nextProps.errors) {
+            this.setState({ errors: nextProps.errors});
+        }
+    }
+
+    onChange(e) {
+        this.setState({ [e.target.name]: e.target.value});
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+
+        const newBooking = {
+            id: this.state.id
+        }
+
+        this.props.deleteBooking(newBooking);
+    }
 
     render() {
 
@@ -18,9 +50,10 @@ class CartItem extends Component {
                         <img src={Pic} alt="Train Image" />
                     </div>
                     <hr />
-                    <form>
+                    <form onSubmit={this.onSubmit}>
                         <div className="train-detail-slot">
                             <div className="main-badge">
+                                <label className="card-label">Quantity: </label>
                                 <label className="badge dark-green right card-label-right">{cart.quantity}</label>
                             </div>
                             <label className="card-label">Train No:</label>
@@ -29,6 +62,7 @@ class CartItem extends Component {
                             <label className="badge card-label lawrencium">{cart.price}</label>
                         </div>
                         <div className="controls">
+                            <input type="text" hidden name="id" value={this.state.id = cart._id} onChange={this.onChange}/>
                             <input type="submit" className="btn" value="Remove" />
                         </div>
                     </form>
@@ -39,7 +73,12 @@ class CartItem extends Component {
 }
 
 CartItem.propType = {
-    cart: PropTypes.object.isRequired
+    cart: PropTypes.object.isRequired,
+    deleteBooking: PropTypes.func.isRequired
 };
 
-export default CartItem;
+const mapStateToProps = state => ({
+    errors: state.errors
+});
+
+export default connect(mapStateToProps, {deleteBooking})(CartItem);
